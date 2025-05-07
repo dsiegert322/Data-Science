15e6bed14d1063f6517ed008a8b05435612052ae
dsiegert322 <dsiegert.id@gmail.com>
2025-05-07 12:06:25 -0600

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#input funtion (user name)
+    #Print (username)
+#input function (user age)
+    #print (user age)
+#Print (Hello world)
+
+name = input("Enter Your Name")
+age = input("Enter Your Age")
+print(name, age)
+
+print("Hello World")
