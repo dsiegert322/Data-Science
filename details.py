8e0606b2ab795132030c31464686f22d3cc7b570
dsiegert322 <dsiegert.id@gmail.com>
2025-05-07 12:49:29 -0600

    Add files via upload

--- details.py
+++ details.py
@@ -1,3 +1,6 @@
-number = 30
-number_str = "10"
-print(number + int(number_str))
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+house_number = input("Enter Your house number: ")
+street_name = input("Enter your street name: ")
+
+print(f"This is {name}. He is {age} and lives at house number {house_number} on {street_name}.")
